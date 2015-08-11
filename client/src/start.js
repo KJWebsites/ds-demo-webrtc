5234a75859bf35f6d991efa77ab65daca3c8e071
Yasser Fadl <yasser.fadl@gmail.com>
2015-08-11 20:35:43 +0200

    Adding basic 9 person room system

--- client/src/start.js
+++ client/src/start.js
@@ -1,10 +1,12 @@
 function start() {
     var ds = deepstream( 'http://52.28.147.204:6020' );
+    var iam = ds.getUid();
+    window.document.title = iam;
     ds.login( {
-        username: 'ds-webrtc-example-' + ds.getUid()
+        username: 'ds-webrtc-example-' + iam
     } );
 
-    var users;
+    var activeCalls = 0;
     var sourcevid = document.getElementById( 'localvideo' );
 
     var mediaStream = null;
@@ -27,25 +29,53 @@ function start() {
         }
     );
 
-    function onCallEstablished( call, stream ) {
+    for( var i = 0; i < 8; i++ ) {
       var remotevid = document.createElement( 'video' );
-      remotevid.className = "remotevideo";
+      remotevid.id = "remotevideo-" + i;
+      remotevid.className = "remotevideo video-size";
       remotevid.autoplay = true;
       document.body.appendChild( remotevid );
+    }
+
+    function resize() {
+      var width = window.innerWidth;
+      var height = window.innerHeight;
+      var elementWidth = Math.round( ( width - 60 ) / 3 );
+      var elementHeight = Math.round(( height - 60 ) / 3 );
+      
+      var elements = window.document.getElementsByTagName( 'video' );
+      for( var i =0; i < elements.length; i++ ) {
+        elements[ i ].style.width = elementWidth + 'px';
+        elements[ i ].style.height = elementHeight + 'px';
+      }
+    }
+    window.addEventListener("resize", resize);
+    resize();
+
+    function onCallEstablished( call, stream ) {
+      var remotevid = document.getElementById( 'remotevideo-' + activeCalls );
       remotevid.src = window.URL.createObjectURL( event.stream );
+      activeCalls++;
 
       call.on( 'ended', function() {
-        document.body.removeChild( remotevid );
-      })
+        activeCalls--;
+        window.URL.revokeObjectURL( remotevid.src );
+      });
     }
 
     function startApp() {
-        var iam = ( Math.random() * 100 ).toFixed( 0 );
-        
+        var calls = [];
         ds.webrtc.registerCallee( iam, function( call, metadata ) {
             call.on( 'established', onCallEstablished.bind( null, call ) );
             call.accept( mediaStream );
+            calls.push( call );
         } );
+
+        window.addEventListener("unload", function(event) { 
+          for( var i=0; i<calls.length; i++ ) {
+            calls[ i ].end();
+          }
+        }, true);
 
         ds.rpc.make( 'get-random-room', {
             user: iam
--- server/rooms/rooms.js
+++ server/rooms/rooms.js
@@ -1,59 +1,74 @@
 var utils = require( './utils' );
-var maxUsers = 3;
-
-function getUserChanges( oldUsers, newUsers) {
-  var currentUsers = {};
-  var unSubscribedUsers = {};
-  var i, k;
-  for (i=0, len=newUsers.length; i<len; i++) { 
-  	currentUsers[ newUsers[ i ] ] = oldUsers[ newUsers[ i ] ] || { name: newUsers[ i ] }; 
-  }
-  for ( k in oldUsers) { 
-  	if ( typeof currentUsers[ k ] === 'undefined' ) {
-		  unSubscribedUsers[ k ] = oldUsers[ k ];
-	 }
-  }
-  return { currentUsers: currentUsers, unSubscribedUsers: unSubscribedUsers};
-}
-
-function removeUsers( unsubscribedUsers, rooms ) {
-	var user;
-	var room;
-	for( var name in unsubscribedUsers ) {
-		user = unsubscribedUsers[ name ];
-		console.log( 'Should remove', user)
-		if( typeof user.room !== 'undefined' ) {
-			room = rooms[ user.room ];
-			room.users.splice( room.users.indexOf( name ), 1 );
-			utils.log( 'Removing user ' + user.name + ' from room ' + room.name );
-		}
-	}
-}
-
+var maxUsers = 9;
 var roomID = 0;
-function getRandomRoom( rooms, user ) {
-	var room = null;
-	var randomRooms = utils.shuffle( rooms.slice( 0 ) );
-	for( var i = 0; i < randomRooms.length && !room; i++ ) {
-		if( randomRooms[ i ].users.length < maxUsers ) {
-			room = randomRooms[ i ];
-			room.users.push( user );
-			break;
-		}
-	}
-	if( !room ) {
-		room = { name: roomID++, users: [ user ] };
-		rooms.push( room );
-	}
-	user.room = room.name;
-	utils.log( 'Added user ' + user.name + ' to room ' + room.name );
-	utils.log( 'Room ' + room.name + ' now has ' + room.users.length + ' users' );
-	return room;
-}
 
 module.exports = function( ds ) {
 	var rooms = [];
 	var users = {};
+
+	function getUserChanges( oldUsers, newUsers) {
+	  var currentUsers = {};
+	  var unSubscribedUsers = {};
+	  var i, k;
+	  for (i=0, len=newUsers.length; i<len; i++) { 
+	  	currentUsers[ newUsers[ i ] ] = oldUsers[ newUsers[ i ] ] || { name: newUsers[ i ] }; 
+	  }
+	  for ( k in oldUsers) { 
+	  	if ( typeof currentUsers[ k ] === 'undefined' ) {
+			  unSubscribedUsers[ k ] = oldUsers[ k ];
+		 }
+	  }
+	  return { currentUsers: currentUsers, unSubscribedUsers: unSubscribedUsers};
+	}
+
+	function removeUsers( unsubscribedUsers, rooms ) {
+		var user;
+		var room;
+		for( var name in unsubscribedUsers ) {
+			user = unsubscribedUsers[ name ];
+			if( typeof user.room !== 'undefined' ) {
+				room = rooms[ user.room ];
+				user = room.users.splice( room.users.indexOf( name ), 1 );
+				utils.log( 'Removing user ' + name + ' from room ' + room.name );
+				utils.log( 'Room ' + room.name + ' now has ' + room.users.length + ' users' + ' : ' + JSON.stringify( room.users ) );
+			}
+		}
+	}
+
+	function addUserToRoom( room, user) {
+		room.users.push( user.name );
+		user.room = room.name;
+		utils.log( 'Added user ' + user.name + ' to room ' + room.name );
+		utils.log( 'Room ' + room.name + ' now has ' + room.users.length + ' users : ' + room.users.join( ',' ) );
+	}
+
+	function getRoom( sortedRooms ) {
+		var room = null;
+		
+		for( var i = 0; i < sortedRooms.length && !room; i++ ) {
+			if( sortedRooms[ i ].users.length < maxUsers ) {
+				room = sortedRooms[ i ];
+				break;
+			}
+		}
+		if( !room ) {
+			room = { name: roomID++, users: [] };
+			rooms.push( room );
+		}
+		return room;
+	}
+
+	function validate( data ) {
+		if( typeof data.user === 'undefined' ) {
+			return 'Please provider a username';
+		}
+
+		if( typeof users[ data.user ] === 'undefined' ) {
+			return 'Please register user as a callee';
+		}
+
+		return null;
+	}
 
 	ds.webrtc.listenForCallees( function( callees ) {
 		var userChanges = getUserChanges( users, callees );
@@ -62,19 +77,16 @@ module.exports = function( ds ) {
 	} );
 
 	ds.rpc.provide( 'get-random-room', function( data, response ) {
-		var user = data.user;
-		var result = [];
 		var room;
-
-		if( !user ) {
-			response.error( 'Missing User' );
+		var randomRooms = utils.shuffle( rooms.slice( 0 ) );
+		var validationMessage = validate( data );
+		if( validationMessage ) {
+			response.error( validation );
 		}
 
-		room = getRandomRoom( rooms, users[ user ] );
-		
-		for( var i=0; i< room.users.length; i++) {
-			result.push( room.users[ i ].name );
-		} 
-		response.send( result );
+		room = getRoom( randomRooms );
+		addUserToRoom( room, users[ data.user ] )
+
+		response.send( room.users );
 	} );
 };
